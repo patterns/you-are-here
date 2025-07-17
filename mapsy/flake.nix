ebdf02ec7c7ed10301bfcf5a3efbab25767b80a9
興怡 <>
2025-07-17 14:49:13 -0700

    more minimal flake

--- mapsy/flake.nix
+++ mapsy/flake.nix
@@ -1,16 +1,18 @@
 {
-  description = "mini flake";
+  description = "hello flake";
 
   inputs = {
-    nixpkgs.url = "https://channels.nixos.org/nixos-25.05/nixexprs.tar.xz";
+    ####nixpkgs.url = "https://channels.nixos.org/nixos-25.05/nixexprs.tar.xz";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, ... }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
 
   };
+
 }
