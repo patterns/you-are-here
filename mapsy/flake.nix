d6ba6cd7065d9be66a5bd6cf38729d80d1a26913
興怡 <>
2025-07-17 15:27:24 -0700

    can we list platforms and start deps?

--- mapsy/flake.nix
+++ mapsy/flake.nix
@@ -7,12 +7,32 @@
 
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, ... }@inputs:
+  let
+      ####systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+  in
+  {
+    ##packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    ##defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+      packages = systems (pkgs: {
+        default = pkgs.hello;
+      });
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+      devShells = systems (pkgs: {
+        default = pkgs.mkShell (
+          with pkgs;
+          {
+            buildInputs = [
+              zig
+              ####zls
+              # You can add `just` to invoke `zig run`, `zig build-exe`, etc
+              # with specific arguments.
+              ####cairo
+            ];
+          }
+        );
+      });
 
   };
-
 }
