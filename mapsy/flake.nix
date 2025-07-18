cfbb4a33254d1401c97ad1da054659ffcd23ac14
興怡 <>
2025-07-18 15:01:08 -0700

    follow jameswillia.ms linebyline

--- mapsy/flake.nix
+++ mapsy/flake.nix
@@ -4,39 +4,45 @@
   inputs = {
     ####nixpkgs.url = "https://channels.nixos.org/nixos-25.05/nixexprs.tar.xz";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
 
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, ... }:
+    # System types to support.
+    supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
 
   in
   {
-    ####packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    ####defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-        packages.default = pkgs.hello;
-#        packages.default = pkgs.stdenv.mkDerivation {
-#          name = "mapsy";
-#          src = ./.;
-#          buildInputs = [
-#            pkgs.zig
-#          ];
-#        };
+    # Provide some binary packages for selected system types.
+    packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+        # The default package for 'nix build'. This makes sense if the
+        # flake provides only one package or there is a clear "main"
+        # package.
+          default = pkgs.hello;
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            ####pkgs.git
-            ####pkgs.gcc
-            ####pkgs.python3
-            pkgs.zig
-            ####pkgs.cairo
-          ];
-        };
+        });
 
 
-  });
+        # Add dependencies that are only needed for development
+        devShells = forAllSystems (system:
+          let
+            pkgs = nixpkgsFor.${system};
+          in
+          {
+            default = pkgs.mkShell {
+                buildInputs = with pkgs; [ zig zls lldb cairo ];
+          };
+        });
+
+
+  };
 }
 
