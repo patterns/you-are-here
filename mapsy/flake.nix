61599bdf1e8046ef181fa099478c422bab77a594
興怡 <>
2025-07-18 15:04:49 -0700

    let

--- mapsy/flake.nix
+++ mapsy/flake.nix
@@ -8,6 +8,7 @@
   };
 
   outputs = { self, nixpkgs, ... }:
+  let
     # System types to support.
     supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
     # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
